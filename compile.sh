fd30e1b98b81083bcf08296d5475761887e00630
Adriano P. Almeida <adriano.almeida@inpe.br>
2020-08-02 17:29:43 -0300

    Adjust compile script

--- compile.sh
+++ compile.sh
@@ -2,13 +2,17 @@
 
 # Script para compilar o material do curso
 
-# cd src
-# Rscript compile.R
-# cd ../docs
-
-# cp introducao.html index.html
-
-rm -rf docs/*
+#production
 cd src
-R -e "bookdown::render_book('index.Rmd', bookdown::gitbook(lib_dir = 'libs'))"
-google-chrome ../docs/introdução.html
+Rscript compile.R
+cd ../docs
+cp introducao.html index.html
+
+
+# development
+# rm -rf docs/*
+# cd src
+# R -e "bookdown::render_book('index.Rmd', bookdown::gitbook(lib_dir = 'libs'))"
+# cd ..
+# cp docs/introducao.html docs/index.html
+# google-chrome docs/index.html
