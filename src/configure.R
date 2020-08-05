d2b7728a049a5940c811b7368bdd3af374e5ac11
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-08-05 00:33:22 -0300

    adição do rmarkdown

--- src/configure.R
+++ src/configure.R
@@ -2,5 +2,5 @@
 Script de configuração do bookdown
 '
 
-install.packages(c('bookdown', 'glue', 'devtools'))
+install.packages(c('bookdown', 'glue', 'devtools', 'rmarkdown'))
 devtools::install_github('hadley/emo')
