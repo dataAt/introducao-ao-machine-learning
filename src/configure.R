737dd6bb417fb7f1166d13c14f789b1b819e1720
Felipe <felipe.carlos@fatec.sp.gov.br>
2020-09-06 05:24:04 -0300

    Update configure.R

--- src/configure.R
+++ src/configure.R
@@ -2,5 +2,5 @@
 Script de configuração do bookdown
 '
 
-install.packages(c('bookdown', 'glue', 'devtools', 'rmarkdown'))
+install.packages(c('bookdown', 'glue', 'devtools'))
 devtools::install_github('hadley/emo')
